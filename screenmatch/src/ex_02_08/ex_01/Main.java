dd003360bc2aac9ba7c3ef8d1c185314acb6986d
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-02 15:01:36 -0300

    02_08: add code for exercise 3

--- screenmatch/src/ex_02_08/ex_01/Main.java
+++ screenmatch/src/ex_02_08/ex_01/Main.java
@@ -6,21 +6,9 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Produto> produtos = new ArrayList<>();
 
-        Produto produto1 = new Produto();
-        Produto produto2 = new Produto();
-        Produto produto3 = new Produto();
-
-        produto1.setNome("Lápis");
-        produto1.setPreco(0.50);
-        produto1.setQuantidade(4);
-
-        produto2.setNome("Estojo");
-        produto2.setPreco(3.5);
-        produto2.setQuantidade(2);
-
-        produto3.setNome("Resma de papel");
-        produto3.setPreco(23.50);
-        produto3.setQuantidade(2);
+        Produto produto1 = new Produto("Lápis", 0.50, 4);
+        Produto produto2 = new Produto("Estojo", 3.50, 2);
+        Produto produto3 = new Produto("Resma de papel", 23.50, 2);
 
         produtos.add(produto1);
         produtos.add(produto2);
