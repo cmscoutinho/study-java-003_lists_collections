1566761cff299fc2c32307a49ce4b91c68e8ae24
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-01 23:52:55 -0300

    01_10: add code for exercise 2

---
+++ screenmatch/src/ex_01_10/ex_01/Main.java
@@ -0,0 +1,9 @@
+package ex_01_10.ex_01;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Pessoa> listaPessoas = new ArrayList<>();
+    }
+}
