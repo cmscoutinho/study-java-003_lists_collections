be6e38bcedbee9854a3bf15655adab96b1e53cf9
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-05 11:48:07 -0300

    05_02: add getters/setters, toString() and compareTo()

--- screenmatch/src/br/com/alura/desafio/model/Card.java
+++ screenmatch/src/br/com/alura/desafio/model/Card.java
@@ -1,20 +1,56 @@
 package br.com.alura.desafio.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Card {
+    private int id;
     private double limit;
     private double balance;
     private List<Purchase> purchaseList;
 
-    public Card(double limit) {
+    public Card(int id, double limit) {
+        this.id = id;
         this.limit = limit;
         this.balance = limit;
         this.purchaseList = new ArrayList<>();
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
     public void buy(Purchase purchase) {
         purchaseList.add(purchase);
+    }
+
+    @Override
+    public String toString() {
+        return "" + purchaseList;
+    }
+
+    public void printPurchases() {
+        Collections.sort(purchaseList);
     }
 }
